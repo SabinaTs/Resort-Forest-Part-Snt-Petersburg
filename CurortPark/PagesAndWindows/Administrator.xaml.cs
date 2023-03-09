6669d4297cb9e4ccc952e6e6b07a579c5566209d
SabinaTs <125851594+SabinaTs@users.noreply.github.com>
2023-03-09 13:56:34 +0500

    Add files via upload

---
+++ CurortPark/PagesAndWindows/Administrator.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CurortPark.PagesAndWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для Administrator.xaml
+    /// </summary>
+    public partial class Administrator : Window
+    {
+        public Administrator()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManagerEditBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HistoryBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
